1c00b5b389692d7172a0f2e8b9e6fe6bb8dbe1e9
Nitesh Dangi <nitesh.dangi@niteshr042.successivetech.com>
2022-01-11 17:24:30 +0530

    react-client

--- react-client/src/user/Login.js
+++ react-client/src/user/Login.js
@@ -1,83 +1,83 @@
-// import React, { useState} from "react";
+import React, { useState} from "react";
 import { Form, TextField, Button, FormLayout} from '@shopify/polaris';
 import "./Login.css";
 
-// const Login = () => {
+const Login = () => {
 
-//     const [email, setEmail] = useState("");
-//     const [password, setPassword] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState('');
 
-//     const onSubmit = () => {
-//         setEmail("");
-//         setPassword("");
-//         console.log("logined");
-//     }
+    const onSubmit = () => {
+        setEmail("");
+        setPassword("");
+        console.log("logined");
+    }
 
 
-//     return(
-//         <div>
-//             <Form onSubmit={onSubmit}>
-//                 <FormLayout >
-//                    <div  className="form-layout" > 
-//                         <h1 >Welcome to the Login Page</h1>
-//                         <div className="email-field" >
-//                             <TextField  label="Email" autoComplete="off" align="left" type="email" onChange={(e) => setEmail(e)} value={email} />
-//                         </div>
-//                         <div className="password-field" >
-//                             <TextField  label="Password" autoComplete="off" align="left" type="password" onChange={(e) => setPassword(e)} value={password} />
-//                         </div>
-//                         <div  className="btn" >
-//                             <Button  submit > Login </Button>
-//                         </div>
-//                     </div>
-//                 </FormLayout>
-//             </Form>
-//         </div>
-//     );
-// }
+    return(
+        <div>
+            <Form onSubmit={onSubmit}>
+                <FormLayout >
+                   <div  className="form-layout" > 
+                        <h1 >Welcome to the Login Page</h1>
+                        <div className="email-field" >
+                            <TextField  label="Email" autoComplete="off" align="left" type="email" onChange={(e) => setEmail(e)} value={email} />
+                        </div>
+                        <div className="password-field" >
+                            <TextField  label="Password" autoComplete="off" align="left" type="password" onChange={(e) => setPassword(e)} value={password} />
+                        </div>
+                        <div  className="btn" >
+                            <Button  submit > Login </Button>
+                        </div>
+                    </div>
+                </FormLayout>
+            </Form>
+        </div>
+    );
+}
 
-// export default Login;
+export default Login;
 
 
 //  Class based Component --------------------------------------------
 
-import React, {Component} from 'react';
-class Login extends Component {
-constructor(props){
-  super(props);
-  this.state={
-    email:'',
-    password:''
-  }
-  this.onSubmit = this.onSubmit.bind(this);
- }
+// import React, {Component} from 'react';
+// class Login extends Component {
+// constructor(props){
+//   super(props);
+//   this.state={
+//     email:'',
+//     password:''
+//   }
+//   this.onSubmit = this.onSubmit.bind(this);
+//  }
 
- onSubmit(e){
+//  onSubmit(e){
 
-        console.log("logined by :" + this.state.email);
-        this.setState({email: '', password: ''});
-        e.preventDefault();
-    }
-render() {
-    return (
-        <div>
-             <Form onSubmit={this.onSubmit}>
-                 <FormLayout >
-                    <div  className="form-layout" > 
-                         <h1 >Welcome to the Login Page</h1>
-                         <div className="email-field" >
-                             <TextField  label="Email" autoComplete="off" align="left" type="email" onChange={(newValue) => this.setState({email:newValue})} value={this.state.email} />
-                         </div>
-                         <div className="password-field" >
-                             <TextField  label="Password" autoComplete="off" align="left" type="password" onChange={(newValue) => this.setState({password:newValue})} value={this.state.password} />
-                         </div>
-                         <div  className="btn" >
-                             <Button  submit  > Login </Button>
-                         </div>
-                    </div>
-                 </FormLayout>
-            </Form>
-        </div>
+//         window.alert("Email: " + this.state.email + "  Password: " + this.state.password);
+//         this.setState({email: '', password: ''});
+//         e.preventDefault();
+//     }
+// render() {
+//     return (
+//         <div>
+//              <Form onSubmit={this.onSubmit}>
+//                  <FormLayout >
+//                     <div  className="form-layout" > 
+//                          <h1 >Welcome to the Login Page</h1>
+//                          <div className="email-field" >
+//                              <TextField  label="Email" autoComplete="off" align="left" type="email" onChange={(newValue) => this.setState({email:newValue})} value={this.state.email} />
+//                          </div>
+//                          <div className="password-field" >
+//                              <TextField  label="Password" autoComplete="off" align="left" type="password" onChange={(newValue) => this.setState({password:newValue})} value={this.state.password} />
+//                          </div>
+//                          <div  className="btn" >
+//                              <Button  submit  > Login </Button>
+//                          </div>
+//                     </div>
+//                  </FormLayout>
+//             </Form>
+//         </div>
 
 
     //   <div>
@@ -103,8 +103,8 @@ render() {
     //      </div>
     //      </MuiThemeProvider>
     //   </div>
-    );
-  }
-}
+//     );
+//   }
+// }
 
-export default Login;
+// export default Login;
