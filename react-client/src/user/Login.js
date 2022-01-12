f56f964eeab72df14a6c1d19d46db6d25d4ecb07
Nitesh Dangi <nitesh.dangi@niteshr042.successivetech.com>
2022-01-12 16:30:45 +0530

    velidation

--- react-client/src/user/Login.js
+++ react-client/src/user/Login.js
@@ -1,144 +1,144 @@
-import React, { useState} from "react";
+// import React, { useState} from "react";
 import { Form, TextField, Button, FormLayout} from '@shopify/polaris';
 import "./Login.css";
 
 // function component
 
-const Login = () => {
+// const Login = () => {
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState('');
+//     const [emailError, setEmailError] = useState('');
+//     const [passwordError, setPasswordError] = useState('');
 
-    const emailValidation = () => {
-        let REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(email === "") {
-          setEmailError("email is required")
-        }
-        else if(email.match(REGEX)){
-          setEmailError("")
-        }
-        else {
-          setEmailError("please Enter valid Email")
-        }
-      }
-
-
-      const passwordErrorValidation = () => {
-        if(password === ""){
-          setPasswordError('Password is required')
-        }
-        else {
-          setPasswordError("")
-        }
-      }
-     const passwordValidation = () => {
-         let REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$";
-         if(password.match(REGEX)) {
-           return true;
-         }
-         else {
-           return false;
-         }
-       }
+//     const emailValidation = () => {
+//         let REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//         if(email === "") {
+//           setEmailError("email is required")
+//         }
+//         else if(email.match(REGEX)){
+//           setEmailError("")
+//         }
+//         else {
+//           setEmailError("please Enter valid Email")
+//         }
+//       }
 
 
-       const onSubmit = () => {
-        let pass = passwordValidation();
-        if(emailError !== "") {
-          alert('Email is not Valid and not proper format read the instruction carefully.')
-        }
-        else if(!pass) {
-          alert('Password is not Valid because minumum six inputs are allowed. ')
-        }
-        else {
-          window.alert("email:"+ email + "  password: " + password);
-          setEmail('');
-          setPassword('');
-          }
-      }
+//       const passwordErrorValidation = () => {
+//         if(password === ""){
+//           setPasswordError('Password is required')
+//         }
+//         else {
+//           setPasswordError("")
+//         }
+//       }
+//      const passwordValidation = () => {
+//          let REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$";
+//          if(password.match(REGEX)) {
+//            return true;
+//          }
+//          else {
+//            return false;
+//          }
+//        }
 
 
-    // const onSubmit = () => {
-    //     setEmail("");
-    //     setPassword("");
-    //     console.log("logined");
-    // }
+//        const onSubmit = () => {
+//         let pass = passwordValidation();
+//         if(emailError !== "") {
+//           alert('Email is not Valid and not proper format read the instruction carefully.')
+//         }
+//         else if(!pass) {
+//           alert('Password is not Valid because minumum six inputs are allowed. ')
+//         }
+//         else {
+//           window.alert("email:"+ email + "  password: " + password);
+//           setEmail('');
+//           setPassword('');
+//           }
+//       }
 
 
-    return(
-        <div>
-            <Form onSubmit={onSubmit}>
-                <FormLayout >
-                   <div  className="form-layout" > 
-                        <h1 >Welcome to the Login Page</h1>
-                        <div className="email-field" >
-                            <TextField  label="Email" autoComplete="off" align="left" type="email" onChange={(e) => setEmail(e)} onBlur = {emailValidation} value={email} />
-                            <span className = "error">{emailError}</span>
-                        </div>
+//     // const onSubmit = () => {
+//     //     setEmail("");
+//     //     setPassword("");
+//     //     console.log("logined");
+//     // }
+
+
+//     return(
+//         <div>
+//             <Form onSubmit={onSubmit}>
+//                 <FormLayout >
+//                    <div  className="form-layout" > 
+//                         <h1 >Welcome to the Login Page</h1>
+//                         <div className="email-field" >
+//                             <TextField  label="Email" autoComplete="off" align="left" type="email" onChange={(e) => setEmail(e)} onBlur = {emailValidation} value={email} />
+//                             <span className = "error">{emailError}</span>
+//                         </div>
                         
-                        <div className="password-field" >
-                            <TextField  label="Password" autoComplete="off" align="left" type="password" onChange={(e) => setPassword(e)} onBlur = {passwordErrorValidation} value={password} />
-                            <span className = "error">{passwordError}</span>
-                        </div>
+//                         <div className="password-field" >
+//                             <TextField  label="Password" autoComplete="off" align="left" type="password" onChange={(e) => setPassword(e)} onBlur = {passwordErrorValidation} value={password} />
+//                             <span className = "error">{passwordError}</span>
+//                         </div>
                         
-                        <div  className="btn" >
-                            <Button  submit > Login </Button>
-                        </div>
-                    </div>
-                </FormLayout>
-            </Form>
-        </div>
-    );
-}
+//                         <div  className="btn" >
+//                             <Button  submit > Login </Button>
+//                         </div>
+//                     </div>
+//                 </FormLayout>
+//             </Form>
+//         </div>
+//     );
+// }
 
-export default Login;
+// export default Login;
 
 
 //  Class based Component --------------------------------------------
 
-// import React, {Component} from 'react';
-// class Login extends Component {
-// constructor(props){
-//   super(props);
-//   this.state={
-//     email:'',
-//     password:''
-//   }
-//   this.onSubmit = this.onSubmit.bind(this);
-//  }
+import React, {Component} from 'react';
+class Login extends Component {
+constructor(props){
+  super(props);
+  this.state={
+    email:'',
+    password:''
+  }
+  this.onSubmit = this.onSubmit.bind(this);
+ }
 
-//  onSubmit(e){
+ onSubmit(e){
 
-//         window.alert("Email: " + this.state.email + "  Password: " + this.state.password);
-//         this.setState({email: '', password: ''});
-//         e.preventDefault();
-//     }
-// render() {
-//     return (
-//         <div>
-//              <Form onSubmit={this.onSubmit}>
-//                  <FormLayout >
-//                     <div  className="form-layout" > 
-//                          <h1 >Welcome to the Login Page</h1>
-//                          <div className="email-field" >
-//                              <TextField  label="Email" autoComplete="off" align="left" type="email" onChange={(newValue) => this.setState({email:newValue})} value={this.state.email} />
-//                          </div>
-//                          <div className="password-field" >
-//                              <TextField  label="Password" autoComplete="off" align="left" type="password" onChange={(newValue) => this.setState({password:newValue})} value={this.state.password} />
-//                          </div>
-//                          <div  className="btn" >
-//                              <Button  submit  > Login </Button>
-//                          </div>
-//                     </div>
-//                  </FormLayout>
-//             </Form>
-//         </div>
+        window.alert("Email: " + this.state.email + "  Password: " + this.state.password);
+        this.setState({email: '', password: ''});
+        e.preventDefault();
+    }
+render() {
+    return (
+        <div>
+             <Form onSubmit={this.onSubmit}>
+                 <FormLayout >
+                    <div  className="form-layout" > 
+                         <h1 >Welcome to the Login Page</h1>
+                         <div className="email-field" >
+                             <TextField  label="Email" autoComplete="off" align="left" type="email" onChange={(newValue) => this.setState({email:newValue})} value={this.state.email} />
+                         </div>
+                         <div className="password-field" >
+                             <TextField  label="Password" autoComplete="off" align="left" type="password" onChange={(newValue) => this.setState({password:newValue})} value={this.state.password} />
+                         </div>
+                         <div  className="btn" >
+                             <Button  submit  > Login </Button>
+                         </div>
+                    </div>
+                 </FormLayout>
+            </Form>
+        </div>
 
 
-    //     );
-//   }
-// }
+        );
+  }
+}
 
-// export default Login;
+export default Login;
